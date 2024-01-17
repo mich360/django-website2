484fd918c9db5dcde92b9b8002e5e4da7debb562
naomichi masumoto <naomichimasumoto@michi-2022MacBook-Pro.local>
2024-01-17 22:55:22 +0900

    Update settings.py

--- django_website/settings.py
+++ django_website/settings.py
@@ -50,8 +50,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
-
 ROOT_URLCONF = 'django_website.urls'
 
 TEMPLATES = [
@@ -118,13 +118,9 @@ USE_TZ = True
 
 # 静的ファイルの設定
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
